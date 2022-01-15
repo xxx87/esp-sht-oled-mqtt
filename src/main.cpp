078a41d7ad13a61ed589d95d0d77d82e08f1d4c1
illia.is <illia.is@room4.team>
2022-01-15 21:54:11 +0200

    D

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,18 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define INIT_KEY 50              // ключ первого запуска. 0-254, на выбор
 #define WIFI_DATA_START_ADDR 100 // адрес для начала записи/чтения данных о сохранённом ВайФай
 
-unsigned long period_time = (long)5000;
-unsigned long my_timer;
+// unsigned long period_time = (long)5000;
+// unsigned long my_timer;
 // const char *ssid = "ax55_is_home";
 // const char *password = "@lk@tr@s_2020";
+IPAddress WiFiIP;
 EncButton<EB_TICK, 12> resetBtn;
 String wifi_ssid = "Thermostat_ISA";     // имя точки доступа
 String wifi_password = "12345678";       // пароль точки доступа
 ESP8266WebServer server(80);             // веб-сервер на 80 порту
 EepromRWU rwu(512, INIT_ADDR, INIT_KEY); // EEPROM size;
 TimerMs wifiTmr(500, 1, 0);              // Таймер подключения к ВайФай
-TimerMs dataTmr(1000, 1, 0);             // Таймер опроса данных
+TimerMs dataTmr(3000, 1, 0);             // Таймер опроса данных
 TimerMs resetTmr(5000, 1, 0);            // Таймер для кнопки RESET
 int ledPin = 2;                          // led статуса
 int ledState = LOW;                      // Статус диода (по-умолчанию диод выключен)
@@ -62,9 +63,10 @@ void handle_Reload(bool reset = false) {
 }
 
 void handle_Reset() {
-  if (resetBtn.held()) {
-    Serial.println("reset device..."); // однократно вернёт true при удержании
-    display.clearDisplay();            // Clear display buffer
+  if (resetBtn.held()) { // однократно вернёт true при удержании
+    Serial.println("reset device...");
+
+    display.clearDisplay(); // Clear display buffer
     display.setCursor(0, 18);
     display.println("Device reset");
     display.println(" ");
@@ -86,7 +88,7 @@ void handle_SettingsHtmlPage() {
   send_Data(str);
 }
 
-void handle_AccessPoint() {
+void handle_SaveSettingsHtmlPage() {
   display.clearDisplay(); // Clear display buffer
   display.setCursor(0, 18);
   display.println("Settings saved");
@@ -147,7 +149,7 @@ void runAsAp() {
   display.display();
 
   server.on("/", handle_SettingsHtmlPage);
-  server.on("/ok", handle_AccessPoint);
+  server.on("/ok", handle_SaveSettingsHtmlPage);
   server.begin();
 }
 
@@ -170,65 +172,63 @@ void runWebServer() {
       display.display();
     }
   }
+  WiFiIP = WiFi.localIP();
 
-  ledState = HIGH;
-  //
-  display.setCursor(0, 5);
-  display.print(WiFi.localIP());
-  display.drawBitmap(111, 1, wifi1_icon16x16, 16, 16, 1);
-  display.drawLine(0, 16, 124, 16, SSD1306_WHITE);
-  display.display();
-
-  digitalWrite(ledPin, ledState);
+  digitalWrite(ledPin, HIGH);
 
   server.on("/", handle_WebServerOnConnect);
   server.onNotFound(handle_PageNotFound);
   server.begin();
+}
 
-  Serial.println(ssid);
-  Serial.println(pass);
+void prinMainInfo(float temp, float hum) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 5);
+  display.print(WiFiIP);
+  display.drawBitmap(111, 1, wifi1_icon16x16, 16, 16, 1);
+  display.drawLine(0, 16, 124, 16, SSD1306_WHITE);
+  display.drawBitmap(31, 18, temperature_icon16x16, 16, 16, 1);
+  display.drawBitmap(93, 18, humidity2_icon16x16, 16, 16, 1);
+  display.setTextSize(2);
+  display.setCursor(12, 50); // cursor for TEMP
+  display.print(temp, 1);
+  display.setCursor(77, 50); // cursor for HUM
+  display.print(hum, 1);
+  display.display();
 }
 
 void setup() {
-  my_timer = millis(); // "сбросить" таймер
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+
   resetBtn.setHoldTimeout(5000); // для сброса устройства, держать кнопку 5 сек.
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    //    Serial.println("SSD1306 allocation failed");
     for (;;)
       ; // Don't proceed, loop forever
   }
   display.setTextSize(1); // указываем размер шрифта
   display.setTextColor(WHITE);
   display.clearDisplay();
-
-  display.drawBitmap(31, 18, temperature_icon16x16, 16, 16, 1);
-  display.drawBitmap(93, 18, humidity2_icon16x16, 16, 16, 1);
-  // for (byte i = 0; i < 10; i++) {
-  //   display.setCursor(0, 0); // установка курсора в позицию X = 0; Y = 0
-  //   display.print(i);        // записываем в буфер памяти дисплея нашу цифру
-  //   display.display();       // и её выводим на экран
-  //   delay(1000);             // ждём 0.5 секунды
-  // }
-
   display.display();
+
   // int sda = 2;  // qui SDC del sensore
   // int sdc = 14; // qui SDA del sensore
   // Wire.begin(sda, sdc);
 
-  if (rwu.isFirstRun()) { // первый запуск
-    runAsAp();
+  if (rwu.isFirstRun()) { // Если первый запуск
+    runAsAp();            // запускаем как Точку Доступа
   } else {
     runWebServer();
+    prinMainInfo(0, 0);
   }
 }
 
 void loop() {
   server.handleClient();
-  handle_Reset();
-  // Если кнопка сброса зажата 5 сек, сбрасываем устройство.
+  handle_Reset(); // Если кнопка сброса зажата 5 сек, сбрасываем устройство.
 
   resetBtn.tick();
 
@@ -239,12 +239,9 @@ void loop() {
   if (resetBtn.release())
     Serial.println("release");
 
-  // if (dataTmr.tick()) {
-  //   Serial.print(".");
-  //   ledState = !ledState;
-  //   Serial.print(ledState);
-  // digitalWrite(ledPin, 1);
-  // }
+  if (dataTmr.tick()) {
+    prinMainInfo(random(10, 60), random(10, 60));
+  }
 
   // if (millis() - my_timer >= period_time) {
   //   my_timer = millis(); // "сбросить" таймер
